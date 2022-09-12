b74fa54f9f05fdb9300956e585b852caf6a9d6fa
Daniel Svensson <daniel.svensson682@gmail.com>
2022-09-12 13:33:31 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+
+//Daniel Svensson .NET22
+
+// ("\u25A0"); // Black square
+// ("\u25A1"); // White square
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; // forceing unicode to show the squares as i've chosen to represent them in Unicode
+            string ChessNumber = ""; // handles storing the user's input of how meny squares
+            int ConvertedChessNumber = 0;
+            int OnGoingThings = 0; // using int as a navigator in the Switch case for simplicity, 0 is default
+
+            string WhiteSquare = "\u25A1";
+            string BlackSquare = "\u25A0";
+            string CustomWhite = "";
+            string CustomBlack = "";
+
+            ConsoleKey actionKey; // for getting the selected option if they want to have custom squares or not
+
+
+            switch (OnGoingThings)
+            {
+                default:
+                    //normal greeting to user, and the first prompt to start the process
+                    Console.WriteLine("Hello and Welcome to the Chessboard generator, \nplease follow the instructions carefully \n");
+                    Console.WriteLine("Please enter a Number for the chessboard grid");
+                    ChessNumber = Console.ReadLine();
+
+                    while (int.TryParse(ChessNumber, out ConvertedChessNumber) is false)
+                    // checks to see if the entered value is a integer or not, and if not then asks the user again until it is one, Infinite loop potential if they dont follow instructions   
+                    {
+                        Console.WriteLine("That was not a number,\t Please try again..");
+                        ChessNumber = Console.ReadLine();
+
+                    }
+                    
+                    do
+                    {
+                        Console.WriteLine("Do you want to set your squares to a custom shape? [Y/N]"); // setting up a dialog if the user wants custom squares or not.
+                        actionKey = Console.ReadKey(false).Key;
+                        if (actionKey != ConsoleKey.Enter)
+                        {
+                            Console.WriteLine();
+
+                        }
+
+                    } while (actionKey != ConsoleKey.Y && actionKey != ConsoleKey.N); //keeps looping untill acceptable response from user, Yes or No.
+                    
+                    if (actionKey == ConsoleKey.Y) // if Yes, it asks for what symbols for each and captures the chosen one,
+                    {
+                        Console.WriteLine("What Character for white?");
+                        CustomWhite = Console.ReadLine();
+                        Console.WriteLine("What Character for Black?");
+                        CustomBlack = Console.ReadLine();
+                        goto case 1; //  then moves to make the squares
+                    }
+                    else
+                    {
+                    goto case 2; // if no, then use the default from the code
+                    }
+
+                case 1: // custom creation on the symbols inputed by the user
+                    CreateSquares(ConvertedChessNumber, CustomWhite, CustomBlack);
+                    break;
+                case 2: // default creation as user did'nt want custom tiles
+                    CreateSquares(ConvertedChessNumber, WhiteSquare, BlackSquare);
+                    break;
+            }
+
+        }
+
+        // small function to create the squares that is needed in the right positions
+        static void CreateSquares(int numberOfSquares, string squareWhite, string squareBlack)  
+            //pulling in the diffrent variables needed to construct the board.
+        {
+
+            for (int i = 0; i < numberOfSquares ; i++) // doing dubbel for loop to populat the board with the right color
+            {
+                
+                for (int j = 0; j < numberOfSquares; j++) //handles the vertical squares == user's input of how meny squares is printed
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write(squareWhite);
+                    }
+                    else
+                    {
+                        Console.Write(squareBlack);
+                    }
+
+                }
+
+                Console.WriteLine(); // generates a new line to handle horizontal squares after the first pass of the square is done,
+                                     // in reality it becomes a square of squares, I;E a chessboard
+            }
+        }
+    }
+}
---
+++ ChessBoard/ProgramExtra.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+//Daniel Svensson .NET22
+
+namespace ChessBoard
+{
+    class ProgramExtra
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; // forceing unicode to show the squares, and setting a standard in the output
+            string ChessNumber = ""; // handles storing the user's input of how meny squares
+            int ConvertedChessNumber = 0;
+            int OnGoingThings = 0; // using int as a navigator in the 'switch case' for simplicity, 0 is default
+
+            string WhiteSquare = "◻︎"; // ◻︎  \u25A1 
+            string BlackSquare = "◼︎"; // ◼︎  \u25A0 ◼︎
+            string CustomWhite = "";
+            string CustomBlack = "";
+            string[,] Board;
+            bool SelectedCustom = false;
+            bool SelectedPeice = false;
+            string PieceSelection = "";
+            ConsoleKey OptionKey; // for getting the selected option if they want to have custom squares or not
+
+
+            switch (OnGoingThings)
+            {
+                default:
+                    //normal greeting to user, and the first prompt to start the process
+                    Console.WriteLine("Hello and Welcome to the Chessboard generator, \nplease follow the instructions carefully \n");
+                    Console.WriteLine("Please enter a Number for the chessboard grid");
+                    Console.WriteLine("Be Warned, boards above 26 squares is not supported for piece placement in this program");
+                    ChessNumber = Console.ReadLine();
+
+                    while (int.TryParse(ChessNumber, out ConvertedChessNumber) is false)
+                    // checks to see if the entered value is a integer or not, and if not then asks the user again until it is one, Infinite loop potential if they dont follow instructions   
+                    {
+                        Console.WriteLine("That was not a number,\t Please try again..");
+                        ChessNumber = Console.ReadLine();
+                    }
+                    do
+                    {
+                        Console.WriteLine("Do you want to set your squares to a custom shape? [Y/N]\n"); // setting up a dialog if the user wants custom squares or not.
+                        OptionKey = Console.ReadKey(false).Key;
+                        if (OptionKey != ConsoleKey.Enter)
+                        {
+                            Console.WriteLine();
+
+                        }
+
+                    } while (OptionKey != ConsoleKey.Y && OptionKey != ConsoleKey.N); //keeps looping untill acceptable response from user, Yes or No.
+
+                    Board = new string[ConvertedChessNumber, ConvertedChessNumber];
+
+                    if (OptionKey == ConsoleKey.Y)
+                    {
+                        SelectedCustom = true;
+                    }
+
+                    if (ConvertedChessNumber < 26)
+                    {
+                        do
+                        {
+                            Console.WriteLine("Do you want to place a piece on the board? [Y/N]\n"); // setting up a dialog if the user wants to place a piece or not.
+                            OptionKey = Console.ReadKey(false).Key;
+                            if (OptionKey != ConsoleKey.Enter)
+                            {
+                                Console.WriteLine();
+
+                            }
+
+                        } while (OptionKey != ConsoleKey.Y && OptionKey != ConsoleKey.N); //keeps looping untill acceptable response from user, Yes or No.
+                        if (OptionKey == ConsoleKey.Y)
+                        {
+                            SelectedPeice = true;
+                        }
+                    }
+
+                    if (OptionKey == ConsoleKey.Y && SelectedCustom == true && SelectedPeice == true) // if Yes, it asks for what symbols for each and captures the chosen one,
+                    {
+                        Console.WriteLine("What Character for white?\n");
+                        CustomWhite = Console.ReadLine();
+                        Console.WriteLine("What Character for Black?\n");
+                        CustomBlack = Console.ReadLine();
+                        Console.WriteLine("And where do you want to place the piece?\n");
+                        PieceSelection = Console.ReadLine().ToUpper();
+                        goto case 1; //  then moves to make the squares
+                    }
+                    else if (OptionKey == ConsoleKey.Y && SelectedCustom == false && SelectedPeice == true)
+                    {
+                        Console.WriteLine("Where do you want to place the piece?\n");
+                        PieceSelection = Console.ReadLine();
+                        goto case 2; //  then moves to make the squares
+                    }
+
+                    else
+                    {
+                        goto case 2; // if no, then use the default from the code
+                    }
+
+                case 1: // custom creation on the symbols inputed by the user
+                    Board = CreateSquares(ConvertedChessNumber, CustomWhite, CustomBlack, SelectedPeice, PieceSelection);
+                    break;
+
+                case 2: // default creation as user didn't want custom tiles
+                    Board = CreateSquares(ConvertedChessNumber, WhiteSquare, BlackSquare, SelectedPeice, PieceSelection);
+                    break;
+
+            }
+
+            for (int i = 0; i < ConvertedChessNumber; i++)
+            {
+                for (int j = 0; j < ConvertedChessNumber; j++)
+                {
+                    Console.Write(Board[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        // small method to create the squares that is needed in the right positions 
+        static string[,] CreateSquares(int numberOfSquares, string squareWhite, string squareBlack, bool pieceSelected ,string piecePlacement)  
+            //pulling in the diffrent variables needed to construct the board.
+        {
+            string[,] BoardsArray = new string[numberOfSquares,numberOfSquares];
+            string[] LettersArray = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int RowIndex = 0;
+            int ColumnIndex = 0;
+            
+            for (int i = 0; i < numberOfSquares ; i++) // doing double 'for' loop to populate the board with the right color at the right place
+            {// first loop handles columns of the squares
+                
+                for (int j = 0; j < numberOfSquares; j++) //handles the rows of squares == user's input of how meny squares is printed
+                {
+                    
+                    if ((i + j) % 2 == 0)
+                    {
+                        BoardsArray[i,j] = squareWhite;
+                    }
+                    else
+                    {
+                        BoardsArray[i,j] = squareBlack;
+                    }
+
+                }
+                
+            }
+            if (pieceSelected == true)
+            {
+                if (piecePlacement.Length == 2) { // checks if it's something like "E7"
+                    RowIndex = Array.IndexOf(LettersArray, piecePlacement.Substring(0, 1).ToUpper());
+
+                    ColumnIndex = Convert.ToInt32(piecePlacement.Substring(1, 1));
+                    BoardsArray.SetValue("♜", ColumnIndex -1, RowIndex); // Column needs to be shortened by 1 due to For loop increment is 1 higher for it
+                }
+                if (piecePlacement.Length == 3) // checks if it's something like "E20"
+                {
+                    RowIndex = Array.IndexOf(LettersArray, piecePlacement.Substring(0, 1));
+
+                    ColumnIndex = Convert.ToInt32(piecePlacement.Substring(1, 2));
+                    BoardsArray.SetValue("♜", ColumnIndex - 1, RowIndex);
+                }
+            }
+            return BoardsArray;
+        }
+    }
+}
